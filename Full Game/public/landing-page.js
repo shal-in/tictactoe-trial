0635353eb78544e96eb156f54a3d0cb5bb6dee81
shal-in <shalin.a.ahasan@gmail.com>
2023-03-30 20:38:53 +0100

    landing page structure completed

--- Full Game/public/landing-page.js
+++ Full Game/public/landing-page.js
@@ -1,7 +1,31 @@
 let clientID = null;
 
-let ws = new WebSocket('ws://localhost:3030');
+let localBtn = document.getElementById('local-btn');
+let onlineBtn = document.getElementById('online-btn');
+let joinBtn = document.getElementById('join-btn')
+let startBtnContainer = document.getElementById('start-btn-container')
 
+// localBtn function
+function localBtnAction() {
+    console.log('local');
+}
+
+// joinBtn function
+function joinBtnAction() {
+    console.log('join');
+}
+
+
+// onlineBtn function
+function onlineBtnAction() {
+    console.log('online');
+}
+
+localBtn.addEventListener('click', localBtnAction);
+onlineBtn.addEventListener('click', onlineBtnAction);
+joinBtn.addEventListener('click', joinBtnAction);
+
+let ws = new WebSocket('ws://localhost:3030');
 ws.onmessage = message => {
     const response = JSON.parse(message.data);
 
