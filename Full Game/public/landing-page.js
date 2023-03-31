f7707f25e8c74e41d3eeb581555e1665c15abcb5
shal-in <shalin.a.ahasan@gmail.com>
2023-03-31 01:31:20 +0100

    more work on landing page (local button)

--- Full Game/public/landing-page.js
+++ Full Game/public/landing-page.js
@@ -1,29 +1,60 @@
 let clientID = null;
+let gameID = '';
 
-let localBtn = document.getElementById('local-btn');
-let onlineBtn = document.getElementById('online-btn');
-let joinBtn = document.getElementById('join-btn')
-let startBtnContainer = document.getElementById('start-btn-container')
+const headerContainer = document.getElementById('header-container');
+const localBtn = document.getElementById('local-btn');
+const onlineBtn = document.getElementById('online-btn');
+const joinBtn = document.getElementById('join-btn');
+const joinInput = document.getElementById("join-input");
+const startBtnContainer = document.getElementById('start-btn-container');
 
 // localBtn function
 function localBtnAction() {
     console.log('local');
+    // remove buttons container
+    startBtnContainer.remove();
+
+    // add back button
+    let backBtn = document.createElement('button');
+    backBtn.setAttribute('id', 'back-btn')
+    backBtn.textContent = 'back'
+    headerContainer.style.height = '300px'
+    headerContainer.appendChild(backBtn)
 }
 
 // joinBtn function
 function joinBtnAction() {
+    if (gameID === ''){
+        gameID = joinInput.value;
+        if (gameID !== '') {
+        console.log('button: ' + gameID);
+        }
+    }
     console.log('join');
 }
-
 
 // onlineBtn function
 function onlineBtnAction() {
     console.log('online');
 }
 
+//backBtn function
+function backBtnAction(){
+    console.log('back')
+}
+
 localBtn.addEventListener('click', localBtnAction);
 onlineBtn.addEventListener('click', onlineBtnAction);
 joinBtn.addEventListener('click', joinBtnAction);
+joinInput.addEventListener('keyup', (e) => {
+    if (gameID === '') {
+        if (e.keyCode === 13){
+            gameID = joinInput.value;
+            console.log("enter: " + gameID);
+        }
+    }   
+}
+);
 
 let ws = new WebSocket('ws://localhost:3030');
 ws.onmessage = message => {
