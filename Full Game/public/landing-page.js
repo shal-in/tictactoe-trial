a70b4e8769753a374f0a521fc148c3c5bfad0352
shal-in <shalin.a.ahasan@gmail.com>
2023-03-31 23:39:26 +0100

    creates gameid

--- Full Game/public/landing-page.js
+++ Full Game/public/landing-page.js
@@ -7,45 +7,197 @@ const onlineBtn = document.getElementById('online-btn');
 const joinBtn = document.getElementById('join-btn');
 const joinInput = document.getElementById("join-input");
 const startBtnContainer = document.getElementById('start-btn-container');
+const mainContainer = document.getElementById('main-container');
+const gameBoard = document.createElement('div');
+const ws = new WebSocket('ws://localhost:3030');
 
 // localBtn function
 function localBtnAction() {
     console.log('local');
+    const homeBtn = document.createElement('button');
+    const restartBtn = document.createElement('button');
+
     // remove buttons container
     startBtnContainer.remove();
 
-    // add back button
-    let backBtn = document.createElement('button');
-    backBtn.setAttribute('id', 'back-btn')
-    backBtn.textContent = 'back'
-    headerContainer.style.height = '300px'
-    headerContainer.appendChild(backBtn)
+    // add home & restart button
+    homeBtn.addEventListener('click', homeBtnAction);
+    homeBtn.setAttribute('id', 'home-btn');
+    homeBtn.textContent = 'home';
+
+    restartBtn.addEventListener('click', restartBtnAction);
+    restartBtn.setAttribute('id', 'restart-btn');
+    restartBtn.textContent = 'restart';
+
+    // restyle header container
+    headerContainer.style.height = '145px';
+    headerContainer.style.width = '';
+
+    const headerText = headerContainer.querySelector('#header-text');
+    headerText.style.fontSize =  '100px';
+    headerText.style.height =  '100px';
+    headerText.style.width =  '';
+
+    const subHeaderText = headerContainer.querySelector('#subheader-text');
+    subHeaderText.style.fontSize =  '40px';
+    subHeaderText.textContent = 'by shalin'
+    subHeaderText.style.height =  '40px';
+    subHeaderText.style.width =  '160px';
+    subHeaderText.style.marginLeft =  '2px';
+    subHeaderText.style.marginRight =  'auto';
+
+
+    headerContainer.insertBefore(restartBtn,subHeaderText);
+    headerContainer.insertBefore(homeBtn,subHeaderText);
+    
+    // add grid
+    gameBoardId = gameBoard.id;
+    if (!gameBoardId){
+        gameBoard.setAttribute('id','gameboard');
+        for (let i = 0; i < 9; i++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid');
+
+            gridSquare.setAttribute('id', i.toString());
+            gameBoard.appendChild(gridSquare); 
+        }
+    }
+
+    mainContainer.appendChild(gameBoard);
+    
 }
 
 // joinBtn function
 function joinBtnAction() {
-    if (gameID === ''){
+    console.log('join');
+    if (gameID === '') {
         gameID = joinInput.value;
-        if (gameID !== '') {
-        console.log('button: ' + gameID);
+        console.log("enter: " + gameID);
+    }   
+}
+
+ws.onmessage = message => {
+    const response = JSON.parse(message.data);
+
+    // connect to server
+    if (clientID === null) {
+        if (response.method === 'connect') {
+            clientID = response.clientID;
+            console.log(`client ${clientID} joined!`)
         }
     }
-    console.log('join');
+
+    // create game
+    if (response.method === "create") {
+        gameID = response.game.gameID; 
+        console.log("gameID: " + response.game.gameID)
+    }
 }
+
 
 // onlineBtn function
 function onlineBtnAction() {
     console.log('online');
+
+    const payLoad = {
+        "method": "create",
+        "clientID": clientID
+    }
+
+    ws.send(JSON.stringify(payLoad));
+
+    const homeBtn = document.createElement('button');
+    const restartBtn = document.createElement('button');
+
+    // remove buttons container
+    startBtnContainer.remove();
+
+    // add home & restart button
+    homeBtn.addEventListener('click', e => homeBtnAction());
+    homeBtn.setAttribute('id', 'home-btn');
+    homeBtn.textContent = 'home';
+
+    restartBtn.addEventListener('click', restartBtnAction);
+    restartBtn.setAttribute('id', 'restart-btn');
+    restartBtn.textContent = 'restart';
+
+    // restyle header container
+    headerContainer.style.height = '145px';
+    headerContainer.style.width = '';
+
+    const headerText = headerContainer.querySelector('#header-text');
+    headerText.style.fontSize =  '100px';
+    headerText.style.height =  '100px';
+    headerText.style.width =  '';
+
+    const subHeaderText = headerContainer.querySelector('#subheader-text');
+    subHeaderText.style.fontSize =  '40px';
+    subHeaderText.textContent = 'by shalin'
+    subHeaderText.style.height =  '40px';
+    subHeaderText.style.width =  '160px';
+    subHeaderText.style.marginLeft =  '2px';
+    subHeaderText.style.marginRight =  'auto';
+
+
+    headerContainer.insertBefore(restartBtn,subHeaderText);
+    headerContainer.insertBefore(homeBtn,subHeaderText);
+
+    // add grid
+    gameBoardId = gameBoard.id;
+    if (!gameBoardId){
+        gameBoard.setAttribute('id','gameboard');
+        for (let i = 0; i < 9; i++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid');
+
+            gridSquare.setAttribute('id', i.toString());
+            gameBoard.appendChild(gridSquare); 
+        }
+    }
+
+    mainContainer.appendChild(gameBoard);
 }
 
-//backBtn function
-function backBtnAction(){
-    console.log('back')
+// homeBtn function
+function homeBtnAction(){
+    console.log('home');
+    
+    // remove home & restart button
+    const homeBtn = headerContainer.querySelector('#home-btn');
+    const restartBtn = headerContainer.querySelector('#restart-btn');
+    homeBtn.remove()
+    restartBtn.remove()
+
+    // add button container
+    mainContainer.appendChild(startBtnContainer);
+
+    // restyle header container
+    headerContainer.style.height = '212px';
+    headerContainer.style.width = '';
+
+    const headerText = headerContainer.querySelector('#header-text');
+    headerText.style.fontSize =  '150px';
+    headerText.style.height =  '140px';
+    headerText.style.width =  '';
+
+    const subHeaderText = headerContainer.querySelector('#subheader-text');
+    subHeaderText.style.fontSize =  '70px';
+    subHeaderText.textContent = 'made by shalin'
+    subHeaderText.style.height =  '70px';
+    subHeaderText.style.width =  '457px';
+    subHeaderText.style.margin = 'auto';
+
+    gameBoard.remove()
 }
 
-localBtn.addEventListener('click', localBtnAction);
-onlineBtn.addEventListener('click', onlineBtnAction);
-joinBtn.addEventListener('click', joinBtnAction);
+// restartBtn function
+function restartBtnAction(){
+    console.log('restart');
+}
+
+localBtn.addEventListener('click', e => localBtnAction());
+onlineBtn.addEventListener('click', e => onlineBtnAction());
+joinBtn.addEventListener('click', e => joinBtnAction());
 joinInput.addEventListener('keyup', (e) => {
     if (gameID === '') {
         if (e.keyCode === 13){
@@ -53,16 +205,5 @@ joinInput.addEventListener('keyup', (e) => {
             console.log("enter: " + gameID);
         }
     }   
-}
-);
+});
 
-let ws = new WebSocket('ws://localhost:3030');
-ws.onmessage = message => {
-    const response = JSON.parse(message.data);
-
-    // connect to server
-    if (response.method === 'connect') {
-        clientID = response.clientID;
-        console.log(`client ${clientID} joined!`)
-    }
-}
--- Full Game/server/server.js
+++ Full Game/server/server.js
@@ -29,6 +29,7 @@ const httpServer = http.createServer();
 httpServer.listen(3030, () => console.log('Listening on port 3030!'));
 
 const clients = {};
+const games = {};
 
 const wsServer = new websocketServer({
     'httpServer' : httpServer
@@ -45,14 +46,30 @@ wsServer.on('request', request => {
         // message received from client
         
         // creating a new game
+        if (result.method === "create") {
+            const clientID = result.clientID;
+            const gameID = createGameID();
+            games[gameID] = {
+                "gameID": gameID,
+                "clients": []
+            }
+
+            const payLoad = {
+                "method": "create",
+                "game": games[gameID]
+            }
+
+            const con = clients[clientID].connection;
+            con.send(JSON.stringify(payLoad));
+        }
 
 
     })
 
 
 
-    // generate a new cliendID
-    const clientID = generateClientID();
+    // generate a new clientID
+    const clientID = createClientID();
     clients[clientID] = {
         'connection': connection
     };
@@ -67,10 +84,82 @@ wsServer.on('request', request => {
 
 
 })
-
-function generateClientID() {
-    // Generate a random 8-character string of letters and numbers
+// function to create a clientID: random 8-character string of letters and numbers
+function createClientID() {
+    
     const randomString = Math.random().toString(36).substring(2, 10);
     return randomString;
   }
+
+// function to create gameID: "word1-word2-word3" form
+function createGameID() {
+
+    const words = ["act", "add", "age", "aid", "aim", "air", "and", "ant", "any", "ape",
+            "apt", "arc", "are", "ark", "arm", "art", "ash", "ask", "ate", "awe",
+            "axe", "aye", "bad", "bag", "ban", "bar", "bat", "bay", "bed", "bee", 
+            "beg", "bet", "bib", "bid", "big", "bin", "bit", "boa", "bob", "bog", 
+            "boo", "bow", "box", "boy", "bra", "bud", "bug", "bum", "bun", "bus", 
+            "but", "buy", "bye", "cab", "can", "cap", "car", "cat", "cod", "cog", 
+            "col", "con", "coo", "cop", "cot", "cow", "coy", "cry", "cub", "cue", 
+            "cup", "cut", "dad", "dam", "day", "den", "dew", "did", "die", "dig", 
+            "dim", "din", "dip", "doe", "dog", "don", "dot", "dry", "dub", "due", 
+            "dug", "dun", "ear", "eat", "eel", "egg", "ego", "eke", "elf", "elk", 
+            "elm", "end", "eon", "era", "eve", "ewe", "eye", "fad", "fan", "far", 
+            "fat", "fax", "fed", "fee", "fen", "few", "fib", "fig", "fin", "fir", 
+            "fit", "fix", "flu", "fly", "fog", "for", "fox", "fry", "fun", "fur", 
+            "gab", "gag", "gal", "gap", "gas", "gay", "gel", "gem", "get", "gig", 
+            "gin", "got", "gum", "gun", "gut", "guy", "gym", "had", "hag", "ham", 
+            "has", "hat", "hay", "hem", "hen", "her", "hey", "hid", "him", "hip", 
+            "his", "hit", "hog", "hop", "hot", "how", "hub", "hue", "hug", "huh", 
+            "hum", "hut", "ice", "icy", "ill", "ink", "inn", "ion", "ire", "irk", 
+            "its", "ivy", "jab", "jar", "jaw", "jay", "jet", "jew", "jig", "job", 
+            "jog", "jot", "joy", "jug", "jut", "keg", "key", "kid", "kin", "kit", 
+            "lab", "lad", "lag", "lap", "law", "lax", "lay", "lea", "led", "lee", 
+            "leg", "let", "lid", "lie", "lip", "lit", "lob", "log", "loo", "lop", 
+            "lot", "low", "lox", "lug", "lye", "mad", "man", "map", "mat", "maw", 
+            "may", "men", "met", "mew", "mid", "mix", "mob", "mod", "mom", "moo", 
+            "mop", "mow", "mud", "mug", "mum", "nab", "nag", "nap", "nay", "net", 
+            "new", "nip", "nit", "nix", "nod", "non", "nor", "not", "now", "nun", 
+            "nut", "oaf", "oak", "oar", "oat", "odd", "off", "oft", "oil", "old", 
+            "one", "ooh", "opt", "orb", "ore", "our", "out", "owe", "owl", "own", 
+            "pad", "pal", "pan", "pap", "par", "pat", "paw", "pay", "pea", "peg", 
+            "pen", "pep", "per", "pet", "pew", "phi", "pic", "pie", "pig", "pin", 
+            "pip", "pit", "ply", "pod", "pop", "pot", "pow", "pro", "pry", "pub", 
+            "pug", "pun", "pup", "pus", "put", "qua", "rad", "rag", "ran", "rap", 
+            "rat", "raw", "ray", "red", "ree", "ref", "rem", "rep", "rev", "rid", 
+            "rig", "rim", "rip", "rob", "rod", "roe", "rot", "row", "rub", "rue", 
+            "rug", "rum", "run", "rut", "rye", "sac", "sad", "sag", "sal", "sap", 
+            "sat", "saw", "say", "sea", "see", "set", "sew", "she", "shy", "sin", 
+            "sip", "sir", "sit", "six", "ski", "sky", "sly", "sob", "sod", "son", 
+            "sop", "sow", "soy", "spa", "spy", "sty", "sub", "sue", "sum", "sun", 
+            "sup", "tab", "tad", "tag", "tan", "tap", "tar", "tax", "tea", "ted", 
+            "tee", "ten", "the", "tho", "thy", "tic", "tie", "tin", "tip", "toe", 
+            "tog", "tom", "ton", "too", "top", "tot", "tow", "toy", "try", "tub", 
+            "tug", "two", "ugh", "ump", "urn", "use", "van", "vat", "vet", "vie", 
+            "vim", "von", "vow", "wad", "wag", "wan", "war", "was", "wax", "way", 
+            "web", "wed", "wee", "wet", "who", "why", "wig", "win", "wit", "wok", 
+            "won", "woo", "wow", "wry", "wye", "yak", "yam", "yap", "yaw", "yea", 
+            "yen", "yep", "yes", "yet", "yew", "yin", "yon", "you", "yow", "yuk", 
+            "yum", "zap", "zen", "zig", "zip", "zit", "zoo"];
+
+        let index = [];
+        let gameId = "";
+        while (index.length !== 3) {
+            x = Math.floor(Math.random() * words.length);
+            if (index.includes(x)) {
+                continue;
+            }
+            index.push(x);
+        }
+        for (let i of index) {
+            if (i !== index[2]) {
+            gameId += words[i] + "-";
+            }
+            else{
+                gameId += words[i];
+            }
+        }
+
+    return gameId;
+}
   
